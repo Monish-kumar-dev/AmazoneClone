f93eaa01ac892e1f93f8f48478c69f6b04321ee3
Monish-kumar-dev <monishhkumars@gmail.com>
2024-10-04 21:38:31 +0530

    Initial commit

--- force-app/main/default/lwc/coffeeShopHomePage/coffeeShopHomePage.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class CoffeeShopHomePage extends LightningElement {
-    // Add any JavaScript functionality if needed, like form submission, etc.
-}
